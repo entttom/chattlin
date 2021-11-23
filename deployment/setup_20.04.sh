650e1db7f4a3a7b0161206981779e71c7ed2e52b
entttom <entner.tom@gmail.com>
2021-11-23 20:24:25 +0100

    Update setup_20.04.sh

--- deployment/setup_20.04.sh
+++ deployment/setup_20.04.sh
@@ -67,7 +67,7 @@ rvm autolibs disable
 rvm install "ruby-3.0.2"
 rvm use 3.0.2 --default
 
-git clone https://github.com/entttom/maas/maas.git
+git clone https://github.com/entttom/maas.git
 cd maas
 if [[ -z "$1" ]]; then
 git checkout master;
